3787a9231423f0516b314175777cc24684a0913c
Ben Rudhart <hi@benrudhart.com>
2023-12-21 08:06:29 +0100

    Update platform targets

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,7 @@ let package = Package(
     // platforms is set so you can include this package in projects that target iOS 13/macOS 10.15/tvOS 13 without
     // getting errors, but the code in it is marked avaliable only for macOS 11 and iOS 14.
     // It compiles and the tests pass on tvOS 14, but I haven't used it in a tvOS app.
-    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v7), .tvOS(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v17), .watchOS(.v10), .tvOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
