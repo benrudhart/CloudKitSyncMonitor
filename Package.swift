c57f912e6cdc05a50357c77cac0702f1aababd75
Ben Rudhart <hi@benrudhart.com>
2024-06-28 12:28:29 +0200

    Enable Swift StrictConcurrency
    See guide for more info: https://www.swift.org/migration/documentation/swift-6-concurrency-migration-guide/completechecking#In-a-SwiftPM-package-manifest

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,7 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CloudKitSyncMonitor",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
+        ),
         .testTarget(
             name: "CloudKitSyncMonitorTests",
             dependencies: ["CloudKitSyncMonitor"]),
